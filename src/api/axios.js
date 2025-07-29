52ad754b8d141460c98258a560906f5632274841
KFThwala <kagithwala@gmail.com>
2025-07-30 01:47:54 +0200

    baseUrl bug fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const API = axios.create({
 
-  baseURL: VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+
   // baseURL: "http://localhost:3000/api",
   
 });
