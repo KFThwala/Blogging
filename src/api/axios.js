75700afb6d10679b51e2f2eef2a513b233768aa7
KFThwala <kagithwala@gmail.com>
2025-07-30 01:45:27 +0200

    add baseUrl to env file for vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API = axios.create({
 
-  baseURL: 'https://bloggin-backend-c4l0.onrender.com/api',
+  baseURL: VITE_API_BASE_URL
   // baseURL: "http://localhost:3000/api",
   
 });
